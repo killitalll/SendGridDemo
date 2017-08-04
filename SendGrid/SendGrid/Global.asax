85bfb1134e6ffe9624aae4ff6de041624014e450
Unknown <arad18@student.monash.edu>
2017-08-04 13:11:32 +0200

    Starting the SendGrid project and enabling ssl functionality

---
+++ SendGrid/SendGrid/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SendGrid.MvcApplication" Language="C#" %>
---
+++ SendGrid/SendGrid/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SendGrid.Startup))]
+namespace SendGrid
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
